d9ee286b9d61b90ddc994e3a14b8caae2117e314
Иван Провоторов <ivanpro2010@yandex.ru>
2022-11-24 00:58:22 +0300

    47

---
+++ 47/Program.cs
@@ -0,0 +1,36 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.Write("Введите колличество строк: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Введите колличество колонок: ");
+int columns = int.Parse(Console.ReadLine());
+
+double[,] matrixFyrst = new double[rows, columns];
+
+void FillMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = (new Random().NextDouble()) + (new Random().Next(-10, 10));
+        }
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    const int cellWidth = 6;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],cellWidth:f2}");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillMatrix(matrixFyrst);
+PrintMatrix(matrixFyrst);
